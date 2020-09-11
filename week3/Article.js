c63955e714e103c6c3c42392df6e1c7ab0ef437c
Preston Foshee <prestonfoshee@knights.ucf.edu>
2020-09-10 22:45:42 -0400

    Final debugged code
    Fixed error "ReferenceError: Must call super constructor in derived class before accessing 'this' or returning from derived constructor" by adding super(props); and putting ${} around each template literal.

--- week3/Article.js
+++ week3/Article.js
@@ -4,22 +4,25 @@ import Component from './Component.js';
 // Article is a child object of Component
 class Article extends Component {
 
-  constructor (props) {
+  // added super(props); to get rid of error requiring super constructor before accessing "this" later in the program
+  constructor(props) {
+    super(props);
     // Send what it gets to its parent object
   }
 
-  render () {
+  render() {
     // Return HTML with the title, author, and text of this article
     // Use template literals for the variables
+    // added ${} to each expression, allowing the values to be passed through
     return (`
       <Article>
-        <Title>this.props.title</Title>
-        <Author>this.props.author</Author>
-        <Text>this.props.text</Text>
+        <Title>${this.props.title}</Title>
+        <Author>${this.props.author}</Author>
+        <Text>${this.props.text}</Text>
       </Article>
     `
     );
-    
+
   }
 
 }
